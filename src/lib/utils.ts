1b737eb44924b4fca6798e7889e44d9cc51c1362
choinashil <choinashil@gmail.com>
2026-02-26 00:13:19 +0900

    readingTime 계산 유틸 함수 한국어에 맞게 개선

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -7,18 +7,18 @@ export function cn(...inputs: ClassValue[]) {
 
 export function formatDate(date: Date) {
   return date
-    .toLocaleDateString('ko-KR', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
+    .toLocaleDateString("ko-KR", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
     })
-    .replace(/\. /g, '.')
-    .replace(/\.$/, '');
+    .replace(/\. /g, ".")
+    .replace(/\.$/, "");
 }
 
-export function readingTime(html: string) {
-  const textOnly = html.replace(/<[^>]+>/g, '');
-  const wordCount = textOnly.split(/\s+/).length;
-  const readingTimeMinutes = (wordCount / 200 + 1).toFixed();
+export const readingTime = (html: string) => {
+  const textOnly = html.replace(/<[^>]+>/g, "");
+  const charCount = textOnly.replace(/\s/g, "").length;
+  const readingTimeMinutes = Math.ceil(charCount / 600);
   return `${readingTimeMinutes} min read`;
-}
+};
